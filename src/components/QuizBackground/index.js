822016e451fd85665393f42f7beb7f79d8252393
olliveer <j.oliveira90@live.com>
2021-01-26 15:16:25 -0300

    fix background

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -7,7 +7,7 @@ const QuizBackground = styled.div`
   background-size: cover;
   background-image: url(${({ backgroundImage }) => backgroundImage});
   background-color: ${({ theme }) => theme.colors.mainBg};
-  flex: 1;
+  height: 100vh;
   @media screen and (max-width: 500px) {
     background-image: none;
     &:after {

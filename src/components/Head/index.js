14185453c4a8a138c3faf54cd2c7096168b61a8d
olliveer <j.oliveira90@live.com>
2021-01-29 18:25:34 -0300

    fix header thumb

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -15,14 +15,14 @@ function IndexPage() {
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="OverQuiz" />
         <meta property="og:description" content="Um quiz sobre o jogo Overwatch" />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="https://images3.alphacoders.com/702/thumb-1920-702620.png" />
 
         {/* Twitter  */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://metatags.io/" />
         <meta property="twitter:title" content="OverQuiz" />
         <meta property="twitter:description" content="Um quiz sobre o jogo Overwatch" />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="https://images3.alphacoders.com/702/thumb-1920-702620.png" />
       </Head>
     </div>
   );
